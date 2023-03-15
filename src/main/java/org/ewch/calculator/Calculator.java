c80be93149985ef2dff8c58e613b30b068bc1f7e
Eimer Castro <Eimer.Castro@nordstrom.com>
2023-03-15 17:12:19 -0500

    Adding intermediate logging of math operations.

--- src/main/java/org/ewch/calculator/Calculator.java
+++ src/main/java/org/ewch/calculator/Calculator.java
@@ -2,25 +2,43 @@ package org.ewch.calculator;
 
 import java.math.BigInteger;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Calculator {
 
-    public static BigInteger sum(BigInteger  numberA, BigInteger  numberB) {
-        return numberA.add(numberB);
+    private static Logger logger = Logger.getLogger(Calculator.class.getName());
+
+    public static BigInteger sum(BigInteger numberA, BigInteger numberB) {
+        BigInteger result = numberA.add(numberB);
+        logger.log(Level.INFO,
+                "Summing: ".concat(numberA.toString()).concat(" + ").concat(numberB.toString()).concat(" = ").concat(result.toString()));
+        return result;
     }
 
     public static BigInteger subtract(BigInteger numberA, BigInteger numberB) {
-        return numberA.subtract(numberB);
+        BigInteger result = numberA.subtract(numberB);
+        logger.log(Level.INFO,
+                "Subtracting: ".concat(numberA.toString()).concat(" - ").concat(numberB.toString()).concat(" = ").concat(result.toString()));
+        return result;
     }
 
     public static BigInteger multiplication(BigInteger numberA, BigInteger numberB) {
-        return numberA.multiply(numberB);
+        BigInteger result = numberA.multiply(numberB);
+        logger.log(Level.INFO,
+                "Multiplying: ".concat(numberA.toString()).concat(" * ").concat(numberB.toString()).concat(" = ").concat(result.toString()));
+        return result;
     }
 
     public static BigInteger division(BigInteger numberA, BigInteger numberB) {
         if (Objects.equals(numberB, BigInteger.ZERO)) {
-            throw new ArithmeticException("Division by Zero not allowed it.");
+            System.out.println("ERROR: Division by Zero not allowed it.");
+            logger.log(Level.SEVERE, "ERROR: Division by Zero not allowed it.");
+            return BigInteger.ZERO;
         }
-        return numberA.divide(numberB);
+        BigInteger result = numberA.divide(numberB);
+        logger.log(Level.INFO,
+                "Dividing: ".concat(numberA.toString()).concat(" / ").concat(numberB.toString()).concat(" = ").concat(result.toString()));
+        return result;
     }
 }
